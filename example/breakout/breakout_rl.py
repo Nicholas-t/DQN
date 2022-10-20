f252b45682cb6cfd32c64ab32f021914d1ce3cac
Nicholas-t <nicholasbudiharsa@gmail.com>
2022-10-20 16:40:02 +0200

    refactor for gym

--- example/breakout/breakout_rl.py
+++ example/breakout/breakout_rl.py
@@ -2,21 +2,18 @@
 
 #Import the pygame library and initialise the game engine
 import pygame
+from random import randint
 from pygame.locals import (
     K_LEFT,
     K_RIGHT
 )
 #Let's import the Paddle Class & the Ball Class
-from paddle import Paddle
-from ball import Ball
-from brick import Brick
+from example.breakout.paddle import Paddle
+from example.breakout.ball import Ball
+from example.breakout.brick import Brick
 
 class Game:
-    def __init__(
-        self,
-        mode='human',
-        lives=3
-    ):
+    def __init__( self, mode='human',  lives=3):
         pygame.init()
         # Define some colors
         self.WHITE = (255,255,255)
@@ -25,6 +22,32 @@ class Game:
         self.RED = (255,0,0)
         self.ORANGE = (255,100,0)
         self.YELLOW = (255,255,0)
+        self.GREEN = (0, 255, 0)
+        self.BRIGHT_BLUE = (0, 255, 255)
+        self.COLORS = [
+            self.RED,
+            self.ORANGE,
+            self.YELLOW,
+            self.GREEN,
+            self.BRIGHT_BLUE
+        ]
+
+        self.PADDLE_WIDTH = 100
+        self.PADDLE_HEIGHT = 10
+        self.PADDLE_INITIAL_POSITION_X = 350
+        self.PADDLE_INITIAL_POSITION_Y = 700
+        self.BALL_DIMENSION = 10
+        self.BALL_INITIAL_POSITION_X = 345
+        self.BALL_INITIAL_POSITION_Y = 700
+
+        self.BRICK_WIDTH = 50
+        self.BRICK_HEIGHT = 10
+        self.BRICK_N_COLUMN = 10
+        self.BRICK_N_ROW = 20
+        self.BRICK_MARGIN = 60
+        self.BRICK_MARGIN_COLUMN = 20
+        self.BRICK_MARGIN_ROW = 5
+
         self.score = 0
         self.lives = lives
         self.mode = mode
@@ -34,43 +57,34 @@ class Game:
             self.screen_mode = pygame.HIDDEN
 
         # Open a new window
-        self.screen = pygame.display.set_mode((800, 600))
+        self.size = (800, 800)
+        self.screen = pygame.display.set_mode(self.size)
         pygame.display.set_caption("Breakout Game")
 
         #This will be a list that will contain all the sprites we intend to use in our game.
         self.all_sprites_list = pygame.sprite.Group()
 
         #Create the Paddle
-        self.paddle = Paddle(self.LIGHTBLUE, 100, 10)
-        self.paddle.rect.x = 350
-        self.paddle.rect.y = 560
+        self.paddle = Paddle(self.LIGHTBLUE, self.PADDLE_WIDTH, self.PADDLE_HEIGHT)
+        self.paddle.rect.x = self.PADDLE_INITIAL_POSITION_X
+        self.paddle.rect.y = self.PADDLE_INITIAL_POSITION_Y
         self.all_sprites_list.add(self.paddle)
 
         #Create the ball sprite
-        self.ball = Ball(self.WHITE,10,10)
-        self.ball.rect.x = 345
-        self.ball.rect.y = 195
+        self.ball = Ball(self.WHITE,self.BALL_DIMENSION, self.BALL_DIMENSION)
+        self.ball.rect.x = self.BALL_INITIAL_POSITION_X
+        self.ball.rect.y = self.BALL_INITIAL_POSITION_Y
         self.all_sprites_list.add(self.ball)
 
         self.all_bricks = pygame.sprite.Group()
-        for i in range(7):
-            brick = Brick(self.RED,80,30)
-            brick.rect.x = 60 + i* 100
-            brick.rect.y = 60
-            self.all_sprites_list.add(brick)
-            self.all_bricks.add(brick)
-        for i in range(7):
-            brick = Brick(self.ORANGE,80,30)
-            brick.rect.x = 60 + i* 100
-            brick.rect.y = 100
-            self.all_sprites_list.add(brick)
-            self.all_bricks.add(brick)
-        for i in range(7):
-            brick = Brick(self.YELLOW,80,30)
-            brick.rect.x = 60 + i* 100
-            brick.rect.y = 140
-            self.all_sprites_list.add(brick)
-            self.all_bricks.add(brick)
+        for j in range(self.BRICK_N_ROW):
+            for i in range(self.BRICK_N_COLUMN):
+                brick = Brick(self.COLORS[j % len(self.COLORS)],self.BRICK_WIDTH,self.BRICK_HEIGHT)
+                brick.rect.x = self.BRICK_MARGIN + i* (self.BRICK_WIDTH + self.BRICK_MARGIN_COLUMN)
+                brick.rect.y = self.BRICK_MARGIN + j* (self.BRICK_HEIGHT + self.BRICK_MARGIN_ROW)
+                self.all_sprites_list.add(brick)
+                self.all_bricks.add(brick)
+
         # The clock will be used to control how fast the screen updates
         self.clock = pygame.time.Clock()
 
@@ -93,16 +107,23 @@ class Game:
             self.paddle.moveRight(5)
         if action == 2:
             self.paddle.moveLeft(5)
+        # To make sure it doesnt do endless loop
+        if self.ball.velocity[1] == 0:
+            self.ball.velocity[1] = 1
         # --- Game logic should go here
         self.all_sprites_list.update()
         #Check if the ball is bouncing against any of the 4 walls:
-        if self.ball.rect.x>=790:
+        if self.ball.rect.x>=800:
             self.ball.velocity[0] = -self.ball.velocity[0]
         if self.ball.rect.x<=0:
             self.ball.velocity[0] = -self.ball.velocity[0]
-        if self.ball.rect.y>590:
+        if self.ball.rect.y>790:
             self.ball.velocity[1] = -self.ball.velocity[1]
             self.lives -= 1
+            self.ball.rect.x = self.BALL_INITIAL_POSITION_X
+            self.ball.rect.y = self.BALL_INITIAL_POSITION_Y
+            self.ball.velocity = [randint(4,8),randint(-8,8)]
+
             if self.lives == 0:
                 #Display Game Over Message for 3 seconds
                 font = pygame.font.Font(None, 74)
@@ -134,11 +155,21 @@ class Game:
                     pygame.time.wait(3000)
                     #Stop the Game
                     self.carryOn=False
+        self.update_screen()
+
+        
+    def turn_on_screen(self):
+        self.screen = pygame.display.set_mode(
+            self.size, 
+            flags=pygame.SHOWN
+        )
+        self.include_info=True
+    
+    def update_screen(self):
         # --- Drawing code should go here
         # First, clear the screen to dark blue.
         self.screen.fill(self.DARKBLUE)
         pygame.draw.line(self.screen, self.WHITE, [0, 38], [800, 38], 2)
-
         #Display the score and the number of lives at the top of the screen
         font = pygame.font.Font(None, 34)
         text = font.render("Score: " + str(self.score), 1, self.WHITE)
